515888d3ab4466bd079e1255f5232386049f0959
estherbina <estherokiri@gmail.com>
2022-03-10 12:47:50 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+int main(void)
+{
+	string str1="Programming is like building a multilingual puzzle";
+	puts(str1);
+	return (0);
+}
