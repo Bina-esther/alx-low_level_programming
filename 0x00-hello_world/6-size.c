8f361bc5883b3e5bf9af196f2c937b1873c687e2
estherbina <estherokiri@gmail.com>
2022-03-11 01:16:07 -0800

    size of variables

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("Size of Integer:%zu bytes\n" sizeof(int));
+printf("Size of Char:%zu bytes\n" sizeof(char));
+printf("Size of Double:%zu bytes\n" sizeof(float));
+return (0);
+}
