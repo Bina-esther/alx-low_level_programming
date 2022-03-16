61ef8a279f999fb719263c231b11f3b88a04de85
estherbina <estherokiri@gmail.com>
2022-03-16 01:10:34 -0700

    putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include<unistd.h>
+#include "main.h"
+/**
+* _putchar outputc
+* character c
+* to stdout
+*/
+int _putchar(char c)
+{
+return (write(1,&c,1));
+}
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return(0);
+}
