7857af22e26ec24674d01ffbf00da01222ceca21
estherbina <estherokiri@gmail.com>
2022-03-16 13:23:42 -0700

    print alphabet ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+/**
+* Define function 
+* print_alphabet_x10
+*/
+#include "main.h"
+/**
+* prints the alphabet
+* ten times
+*/
+void print_alphabet_x10(void)
+{
+int count = 0;
+char letter;
+while (count++ <=9)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+}
+_putchar('\n');
+}
