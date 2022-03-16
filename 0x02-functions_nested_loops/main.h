cda7c52896a1331a1e0dadc8a3ec85fc89a1a090
estherbina <estherokiri@gmail.com>
2022-03-16 08:58:48 -0700

    added print alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,9 @@
 #define MAIN_H
 /**
 * Includes function prototypes
-* of alx programming
+* for the directory
+* 0x002 functions nested loops
 */
 int _putchar(char c);
+void print_alphabet(void);
 #endif
