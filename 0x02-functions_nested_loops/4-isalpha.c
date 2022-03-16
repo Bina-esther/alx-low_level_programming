ce7d3de93db7ca2fe687ea66ee1885f65e2d9c7b
estherbina <estherokiri@gmail.com>
2022-03-16 14:42:02 -0700

    4 isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+/**
+* Name - isalpha
+* purpose - checks for alphabetic character
+*/
+#include "main.h"
+/**
+* Definition
+*/
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z')||(c >= 'A' && c <= 'B'))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
