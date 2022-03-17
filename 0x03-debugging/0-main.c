595e062ba7c314324c3d4b957b76b55ace20a2b2
estherbina <estherokiri@gmail.com>
2022-03-17 01:21:46 -0700

    debug +ve -ve function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* main - teste function that prints if integer is positive or negative
+* Return: 0
+*/
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negatice(i);
+return (0);
+}
