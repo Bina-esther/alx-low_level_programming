6d8119e8b1f17600d2372dc1a8ddaf59723e44c7
Bina-esther <85230274+Bina-esther@users.noreply.github.com>
2022-03-21 18:38:27 +0300

    Update main.h
    update: reset to 98 function prototype

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,6 @@
 #ifndef MAIN_H
 
-
-
 #define MAIN_H
-
-
 
 /**
 
@@ -14,5 +10,7 @@
 * 0x05 pointers arrays and strings
 */
 int _putchar(char c);
+/*updates the value of a parameter*/
+void reset_to_98(int *n);
 
 #endif
