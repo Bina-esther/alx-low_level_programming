7d4e3c5edff46bae643bb433cb703d4a2c3fff6f
estherbina <estherokiri@gmail.com>
2022-04-03 00:40:41 -0700

    0-whatsmyname.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+/**
+* Task 0
+* Program - prints the rogram name followed by a new line
+*/
+#include <stdio.h>
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
