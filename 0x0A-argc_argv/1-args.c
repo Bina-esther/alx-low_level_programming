22bacfc7d065e9d97900e1140da4a4c4e0829f4e
estherbina <estherokiri@gmail.com>
2022-04-03 00:44:57 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* Task 1
+* Program- prints the number of arguments passed into it followed by a new line
+*/
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
