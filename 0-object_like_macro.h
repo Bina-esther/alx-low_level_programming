ed9efcc34416c415a5c44566bf633247c40a7d69
estherbina <estherokiri@gmail.com>
2022-04-12 10:16:46 -0700

    0-object_like_macro.h

---
+++ 0-object_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+/*
+*  Header file that defines
+*  a macro named SIZE as an abbreviation of the token 1024.
+*/
+#define SIZE 1024
+
+#endif
