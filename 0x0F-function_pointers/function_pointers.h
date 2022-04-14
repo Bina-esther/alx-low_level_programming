07d9253eb4a798bc0072ac31a634af1b9fdceb63
estherbina <estherokiri@gmail.com>
2022-04-14 09:42:52 -0700

    function_pointers.h

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+/* Header file containing prototypes for
+*  functions used in 0x0F-function_pointers directory
+*/
+#include <stdlib.h>
+void print_name(char *name, void(*f)(char *));
+
+#endif
